47aafd5d7a30783a895aa0d774168e622f888ba3
Lynx-Lynx <delinne.weird@gmail.com>
2021-08-19 23:04:44 +0300

    optimized images

--- src/components/card.component.jsx
+++ src/components/card.component.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import '../styles/card.style.css';
+import Image from '../img/chuck.png';
 
 export const Card = (props) => (
    <div className='card-container'>
         <img alt='chuck' 
         className='image'
-        src={`https://i.ibb.co/r4rjNBw/chuck.png`} />
+        src={Image} />
         <h2 className='info'> {props.fact.value} </h2>
     </div>
 )
